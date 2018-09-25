6eb7bf2e206da9bfd03ead740ade60ef17ea1edf
Cian Durrigan <cian@adaptis.io>
2018-09-25 17:47:14 +0700

    SI-config-post-navigation: add php and styling for post navigation

--- wp-content/themes/secretireland/inc/template-tags.php
+++ wp-content/themes/secretireland/inc/template-tags.php
@@ -156,3 +156,17 @@ if ( ! function_exists( 'secretireland_post_thumbnail' ) ) :
 		endif; // End is_singular().
 	}
 endif;
+
+/**
+ * Post navigation (previous / next post) for single posts.
+ */
+function secretireland_post_navigation() {
+	the_post_navigation( array(
+		'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'secretireland' ) . '</span> ' .
+			'<span class="screen-reader-text">' . __( 'Next post:', 'secretireland' ) . '</span> ' .
+			'<span class="post-title">%title</span>',
+		'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'secretireland' ) . '</span> ' .
+			'<span class="screen-reader-text">' . __( 'Previous post:', 'secretireland' ) . '</span> ' .
+			'<span class="post-title">%title</span>',
+	) );
+}
