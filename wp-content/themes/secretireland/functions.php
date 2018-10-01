e5480c8b9e42a4cac7de6ddd8074db7a08910d99
Cian Durrigan <cian@adaptis.io>
2018-10-01 17:41:49 +0700

    SI: Add styling and JS for local storage of active nav item class

--- wp-content/themes/secretireland/functions.php
+++ wp-content/themes/secretireland/functions.php
@@ -194,6 +194,8 @@ function secretireland_scripts() {
 
 	wp_enqueue_script( 'secretireland-functions', get_template_directory_uri() . '/js/functions.js', array('jquery'), '20180925', true );
 
+	wp_enqueue_script( 'secretireland-main', get_template_directory_uri() . '/js/main.js', array('jquery'), '20181001', true );
+
 	wp_enqueue_script( 'secretireland-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
---
+++ wp-content/themes/secretireland/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var navItems = document.querySelectorAll('.main-navigation a');
+  if(navItems.length > 0) {
+    var niTitle = [];
+    navItems.forEach(function(elem) {
+      niTitle.push(elem.innerHTML);
+      elem.addEventListener('click', function() {
+        localStorage.setItem('NIState', niTitle.indexOf(elem.innerHTML));
+      });
+    });
+  }
+  // bind to the onload event
+  window.addEventListener('load', function() {
+    navItems[localStorage.getItem('NIState')].setAttribute('class', 'selected');
+  });
+});
